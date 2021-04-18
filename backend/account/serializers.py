b15629c0d31b820b465d5bdc62e6200760e71d41
sjyang <jasysj@gmail.com>
2021-04-18 16:33:13 +0800

    fix: add nested serializers
    add nested serializers for userProfile and admissions

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from . import models
+from django.apps import apps
+from django.conf import settings
 
 class UserProfileSerializer(serializers.ModelSerializer):
     related_user = serializers.ReadOnlyField(source='userprofile.related_user')
+    class Meta:
+        model = models.UserProfile
+        db_table = 't_user_profile'
+        fields = '__all__'
+        lookup_field = 'id'
+        
+class UserProfileNestedSerializer(serializers.ModelSerializer):
+    related_user = serializers.SlugRelatedField(
+        slug_field="username",
+        queryset=apps.get_model(settings.AUTH_USER_MODEL).objects.all(),
+        many=False
+    )
     class Meta:
         model = models.UserProfile
         db_table = 't_user_profile'
