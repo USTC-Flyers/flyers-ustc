2540ee63af6271d40b61a93e57074decb71b6b7a
sjyang <jasysj@gmail.com>
2021-04-29 15:58:34 +0800

    feat: init wiki

--- frontend/src/components/Wiki.vue
+++ frontend/src/components/Wiki.vue
@@ -1,33 +1,34 @@
 <template>
   <div>
     <div v-html="content"></div>
-    <blog></blog>
   </div>
 </template>
 
 <script>
-import Blog from "./Blog"
-import getAllTopic from "@/api/wiki"
+import {getAllTopic, getTopic} from "@/api/wiki"
 export default {
   name: "Wiki",
   components: {
     blog: Blog
   },
-  props
+  props: {
+    id: Number
+  },
   data() {
     return {
+      // !FIXME: scirpt in html check
       content: '<h1>Title</h1>\n<p>content</p>'
     }
   },
   created() {
-    this.getTopics()
+    this.getData(this.id)
   },
   methods: {
-    getTopics() {
-      getAllTopic().then((res) => {
-        
+    getData(id) {
+      getTopic(id).then((res) => {
+        this.content = res.current_version.content
       }).catch((err) => {
-        
+        console.log(err);
       });
     }
   },
