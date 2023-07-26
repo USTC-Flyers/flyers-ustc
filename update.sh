d7287b4f60e1e7ebe2ba4e88529ebd1f9a815afd
lluckydog <zhanghaowen@mail.ustc.edu.cn>
2023-07-26 11:58:38 +0800

    update docker compose

--- update.sh
+++ update.sh
@@ -6,7 +6,6 @@ then
 fi
 cd $1
 git pull origin master
-docker-compose build
 docker-compose down
-docker network rm flyers-ustc_database-network
+# docker-compose build
 docker-compose up -d  --remove-orphans
